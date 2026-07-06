667099b9d8759a5cb78c9df4e5e4e144ccd00221
Aaditya <156561777+aaditya-1801@users.noreply.github.com>
2026-07-06 10:37:12 +0530

    Time: 0 ms (100%), Space: 15.2 MB (60.13%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        vector<vector<int>> res;
+        // vector<vector<int>> res;
 
         int n = intervals.size();
         auto lambda = [](vector<int>& vec1, vector<int>& vec2) {
@@ -13,15 +13,24 @@ public:
 
         sort(intervals.begin(), intervals.end(), lambda);
 
-        res.push_back(intervals[0]);
+        // res.push_back(intervals[0]);
+        int cnt = 1;
+        int left = intervals[0][0];
+        int right = intervals[0][1];
 
-        for(int i=0; i<n; i++){
-            if(res.back()[0] <= intervals[i][0] && res.back()[1] >= intervals[i][1])
+        for(int i=1; i<n; i++){
+            // res.back()[0] <= intervals[i][0] ...this is redundant as we sorted intervals
+            // if(res.back()[0] <= intervals[i][0] && res.back()[1] >= intervals[i][1])
+            if(left <= intervals[i][0] && right >= intervals[i][1])
                 continue;
 
-            res.push_back(intervals[i]);
+            // res.push_back(intervals[i]);
+            cnt += 1;
+            left = intervals[i][0];
+            right = intervals[i][1];
         }
 
-        return res.size();
+        // return res.size(); 
+        return cnt;
     }
 };
