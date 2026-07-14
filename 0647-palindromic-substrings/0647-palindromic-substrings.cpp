7509dbc291fbd1148e50481be5460280b0794cbc
Aaditya <156561777+aaditya-1801@users.noreply.github.com>
2026-07-14 16:19:12 +0530

    Time: 3 ms (85.63%), Space: 8.5 MB (78.51%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,54 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--])
-                return false;
-        }
-        return true;
-    }
-
-    int countSubstrings(string s) {
-        int n = s.size();
+    int expand(string &s, int l, int r) {
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j))
-                    cnt++;
-            }
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            cnt++;
+            l--;
+            r++;
         }
 
         return cnt;
     }
+
+    int countSubstrings(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            // Odd length palindrome
+            ans += expand(s, i, i);
+
+            // Even length palindrome
+            ans += expand(s, i, i + 1);
+        }
+
+        return ans;
+    }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string &s, int l, int r) {
+//         while (l < r) {
+//             if (s[l++] != s[r--])
+//                 return false;
+//         }
+//         return true;
+//     }
+
+//     int countSubstrings(string s) {
+//         int n = s.size();
+//         int cnt = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 if (isPalindrome(s, i, j))
+//                     cnt++;
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
