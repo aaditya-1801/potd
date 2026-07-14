6cd4ed08afb6c7e02084a07dd4d187974dfb2931
Aaditya <156561777+aaditya-1801@users.noreply.github.com>
2026-07-14 11:43:31 +0530

    Time: 1461 ms (13.04%), Space: 90.1 MB (53.91%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -21,7 +21,35 @@ public:
     }
 
     int subsequencePairCount(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, 0, 0);
+        // memset(t, -1, sizeof(t));
+        // return solve(nums, 0, 0, 0);
+
+        int n = nums.size();
+        int maxEl = *max_element(nums.begin(), nums.end());
+
+        int t[n+1][maxEl+1][maxEl+1];
+
+        for(int gcd1 = 0; gcd1 <= maxEl; gcd1++){
+            for(int gcd2 = 0; gcd2 <= maxEl; gcd2++){
+
+                bool bothNonEmpty = (gcd1 != 0 && gcd2 != 0);
+                bool gcdMatched = (gcd1 == gcd2);
+
+                t[n][gcd1][gcd2] = (bothNonEmpty && gcdMatched) ? 1 : 0;
+            }
+        }
+
+        for(int i = n-1; i>=0; i--){
+            for(int gcd1 = maxEl; gcd1 >=0; gcd1--){
+                for(int gcd2 = maxEl; gcd2 >=0; gcd2--){
+                    int skip = t[i+1][gcd1][gcd2];
+                    int take1 = t[i+1][__gcd(gcd1, nums[i])][gcd2];
+                    int take2 = t[i+1][gcd1][__gcd(gcd2, nums[i])];
+
+                    t[i][gcd1][gcd2] = (0LL + skip + take1 + take2) % MOD;
+                }
+            }
+        }
+        return t[0][0][0];
     }
 };
